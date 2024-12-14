cd0885d3633de54075f367503e07db191aeede8f
Mohammad <bhshtbaran@gmail.com>
2024-12-14 12:16:04 -0800

    fix: fix the layout

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -5,14 +5,14 @@ import Sidebar from "@/components/sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="text-white">
+    <div>
       <div className="hidden lg:block">
         <Navbar />
       </div>
       <div className="block lg:hidden">
         <MobileNavbar />
       </div>
-      <div className="flex">
+      <div className="flex mx-auto xl:max-w-[1517px]">
         <Sidebar />
         <section className="min-h-screen flex flex-col flex-1 px-6 pt-28 pb-6 max-md:pb-14 sm:px-14">
           <StreamClientProvider>{children}</StreamClientProvider>
